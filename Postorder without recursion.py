6c13f24b039aa0ea01c5f7aff64eadb230f35121
Abhishek Panigrahi <abhishek2chikun@gmail.com>
2020-09-22 22:44:40 +0530

    Add files via upload

--- Postorder without recursion.py
+++ Postorder without recursion.py
@@ -17,9 +17,31 @@ def post2stack(root):
     while s2:
         temp=s2.pop()
         print(temp.data,end="->")
+def peek(s1):
+    if len(s1)>0:
+        return s1[-1]
+    return None
+def post1stack(root):
+    s1=[]
+    while True:
+        while(root):
+            if root.right is not None:
+                s1.append(root.right)
+            s1.append(root)
+            root=root.left
+        root=s1.pop()
+        if root.right is not None and peek(s1)==root.right:
+            s1.pop()
+            s1.append(root)
+            root=root.right
+        else:
+            print(root.data,end="->")
+            root=None
+        if len(s1)==0:
+            break
 root=Node(1,right=Node(3),left=Node(2,right=Node(5),left=Node(4)))
 print("Postorder using 2 stack:")
 post2stack(root)
-print("\nPreorder with aux space complexity N:")
-post2stack(root)
+print("\nPostorder using ! stack:")
+post1stack(root)
 print()
